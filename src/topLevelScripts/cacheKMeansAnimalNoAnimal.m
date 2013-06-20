ba5fb5872d32689cbd1a7549d9dc3020b629b9c5
Josh Rule <rsj28@georgetown.edu>
2013-06-20 11:14:27 +0700

    setup k-means caching

---
+++ src/topLevelScripts/cacheKMeansAnimalNoAnimal.m
@@ -0,0 +1,19 @@
+home = '/home/joshrule/ruleRiesenhuber2013/';
+addpath(genpath([home 'src/']));
+
+hmaxHome = [home 'src/hmax-ocv/'];
+patchFile = [home 'patchSets/kmeans.xml'];
+maxSize = 240;
+imgDir = [home 'imageSets/animalNoAnimal/'];
+
+animalDir = [imgDir 'animals/'];
+animals = dir([animalDir '*.jpg']);
+animalImgs = strcat(animalDir,{animals.name});
+animalFile = [imgDir 'c2Cache/animals.universalPatches400PerSize.c2.mat'];
+cacheC2(animalFile,patchFile,maxSize,animalImgs,hmaxHome);
+
+noAnimalDir = [imgDir 'noAnimals/'];
+noAnimals = dir([noAnimalDir '*.jpg']);
+noAnimalImgs = strcat(noAnimalDir,{noAnimals.name});
+noAnimalCache = [imgDir 'c2Cache/noAnimals.universalPatches400PerSize.c2.mat'];
+cacheC2(noAnimalCache,patchFile,maxSize,noAnimalImgs,hmaxHome);
