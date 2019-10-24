bd93cbf90b140f768308bda495216a9ee56f1067
Josh Rule <joshua.s.rule@gmail.com>
2019-10-24 07:01:08 -0400

    reorganizing src directory

---
+++ src/fix_image_net_filenames_ilsvrc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /data2/image_sets/image_net/ILSVRC2015/Data/CLS-LOC/val/
+echo `pwd`
+rename 's/.googlenet_cat_mat.mat$/.googlenet_cat_mat/' *.googlenet_cat_mat.mat
+rename 's/.googlenet_gen_mat.mat$/.googlenet_gen_mat/' *.googlenet_gen_mat.mat
+
+for dir in /data2/image_sets/image_net/ILSVRC2015/Data/CLS-LOC/train/*/
+do
+    cd $dir
+    echo `pwd`
+    rename 's/.googlenet_cat_mat.mat$/.googlenet_cat_mat/' *.googlenet_cat_mat.mat
+    rename 's/.googlenet_gen_mat.mat$/.googlenet_gen_mat/' *.googlenet_gen_mat.mat
+done
--- src/top_k.m
+++
@@ -1,9 +0,0 @@
-function topK = top_k(classificationValues,labels,k)
-    [nExamples,nClasses] = size(labels);
-    topKs = nan(nExamples,1);
-    parfor item = 1:nExamples
-        [~,idxs] = sort(classificationValues(item,:),'descend');
-        topKs(item) = ismember(find(labels(item,:)),idxs(1:k));
-    end
-    topK = mean(topKs);
-end
